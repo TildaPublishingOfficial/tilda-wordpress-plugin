9bc80feef6d4379995587c4fe73166695b5155b2
Evgeny Volferts <eugene.wolferz@gmail.com>
2024-01-07 19:04:14 +0300

    Fix cURL encoding

--- class.tilda.php
+++ class.tilda.php
@@ -758,6 +758,7 @@ class Tilda {
 			if ( $curl = curl_init() ) {
 				curl_setopt( $curl, CURLOPT_URL, $url );
 				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+				curl_setopt( $curl, CURLOPT_ENCODING , '' );
 				$out = curl_exec( $curl );
 				curl_close( $curl );
 			} else {
